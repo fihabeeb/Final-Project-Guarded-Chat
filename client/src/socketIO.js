5d32ce38424e6917c0462c48ecdec6dd12c0da08
Fadil Idris <fihabeeb06@gmail.com>
2026-03-08 16:41:05 +0000

    Update socket connection URL to use environment variable for flexibility

--- client/src/socketIO.js
+++ client/src/socketIO.js
@@ -1,6 +1,4 @@
 import { io } from 'socket.io-client'
 
-// Connect to the Express server running on port 1111
-//const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
-const SOCKET_URL = 'http://localhost:5000';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
 export const socket = io(SOCKET_URL);
